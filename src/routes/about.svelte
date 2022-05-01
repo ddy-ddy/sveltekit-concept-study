25f5eb828817b9dab7ca42d14dc074589636642f
Yu Duan <1179730251@qq.com>
2022-05-01 22:49:50 +0800

    1.pages and routes

---
+++ src/routes/about.svelte
@@ -0,0 +1,17 @@
+<script>
+  import About from "./about.svelte";
+</script>
+
+<div class="about">
+  <h3>about us</h3>
+  <p>do you want to know something about me?</p>
+  <a href="/">Home</a>
+</div>
+
+<style>
+  .about {
+    text-align: center;
+    display: block;
+    margin: 20px auto;
+  }
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,23 @@
-<h1>Welcome to SvelteKit</h1>
-<p>
-  Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+<script>
+  let title = "ddy-ddy";
+  const updateTitle = () => {
+    title = "this is my first title.";
+  };
+</script>
+
+<div class="index">
+  <h2>{title}</h2>
+  <p>这就是爱啊啊啊啊啊啊啊</p>
+  <button on:click={updateTitle}>change title</button>
+  <input type="text" bind:value={title} />
+  <a href="/guides">View Guides</a>
+  <a href="/about">About</a>
+</div>
+
+<style>
+  .index {
+    text-align: center;
+    display: block;
+    margin: 30px auto;
+  }
+</style>
